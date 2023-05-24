756b14ecf2197c0170d75300b698fdc892e87a78
unknown <williamcarvalho493@gmail.com>
2023-05-24 19:35:04 -0300

    Criação

--- src/br/ifpr/jogo/modelo/Fase.java
+++ src/br/ifpr/jogo/modelo/Fase.java
@@ -1,1 +1,14 @@
+package br.ifpr.jogo.modelo;
 
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+
+public class Fase {
+    private Image fundo;
+
+    public Fase() {
+        ImageIcon carregando = new ImageIcon("");
+
+    }
+}
