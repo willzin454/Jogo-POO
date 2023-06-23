fd13e7b54eb4e151fe75325bdf32230e180fcdc8
willzin454 <williamcarvalho493@gmail.com>
2023-06-22 22:54:12 -0300

    Criação

--- src/br/ifpr/jogo/modelo/Tiro.java
+++ src/br/ifpr/jogo/modelo/Tiro.java
@@ -1,6 +1,7 @@
 package br.ifpr.jogo.modelo;
 
 import java.awt.Image;
+import javax.swing.ImageIcon;
 
 public class Tiro {
     private int posicaoEmX;
@@ -8,12 +9,61 @@ public class Tiro {
     private Image imagem;
     private int larguraImagem;
     private int alturaImagem;
-    private static final int VELOCIDADE = 2;
+    private static final int VELOCIDADE = 10;
 
     public Tiro(int posicaoPersonagemEmX, int posicaoPersonagemEmY) {
         this.posicaoEmX = posicaoPersonagemEmX;
         this.posicaoEmY = posicaoPersonagemEmY;
     }
 
-    
+    public void carregar() {
+        ImageIcon carregando = new ImageIcon("recursos\\imagen6.jpg");
+        this.imagem = carregando.getImage();
+        this.alturaImagem = this.imagem.getWidth(null);
+        this.larguraImagem = this.imagem.getHeight(null);
+    }
+
+    public void atualizar() {
+        this.posicaoEmX = this.posicaoEmX + VELOCIDADE;
+    }
+
+    public int getPosicaoEmX() {
+        return this.posicaoEmX;
+    }
+
+    public void setPosicaoEmX(int posicaoEmX) {
+        this.posicaoEmX = posicaoEmX;
+    }
+
+    public int getPosicaoEmY() {
+        return this.posicaoEmY;
+    }
+
+    public void setPosicaoEmY(int posicaoEmY) {
+        this.posicaoEmY = posicaoEmY;
+    }
+
+    public Image getImagem() {
+        return this.imagem;
+    }
+
+    public void setImagem(Image imagem) {
+        this.imagem = imagem;
+    }
+
+    public int getLarguraImagem() {
+        return this.larguraImagem;
+    }
+
+    public void setLarguraImagem(int larguraImagem) {
+        this.larguraImagem = larguraImagem;
+    }
+
+    public int getAlturaImagem() {
+        return this.alturaImagem;
+    }
+
+    public void setAlturaImagem(int alturaImagem) {
+        this.alturaImagem = alturaImagem;
+    }
 }
